a122894f1ea888d075e525791ba2136cc211f79a
wcgInHome <milan011@sohu.com>
2023-10-02 09:18:25 +0800

    App init

---
+++ yudao-module-mall/yudao-module-promotion-biz/src/main/java/cn/iocoder/yudao/module/promotion/convert/combination/CombinationActivityConvert.java
@@ -0,0 +1,97 @@
+package cn.iocoder.yudao.module.promotion.convert.combination;
+
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.framework.common.util.collection.CollectionUtils;
+import cn.iocoder.yudao.framework.common.util.collection.MapUtils;
+import cn.iocoder.yudao.module.product.api.spu.dto.ProductSpuRespDTO;
+import cn.iocoder.yudao.module.promotion.api.combination.dto.CombinationRecordCreateReqDTO;
+import cn.iocoder.yudao.module.promotion.api.combination.dto.CombinationRecordRespDTO;
+import cn.iocoder.yudao.module.promotion.controller.admin.combination.vo.activity.CombinationActivityCreateReqVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.combination.vo.activity.CombinationActivityRespVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.combination.vo.activity.CombinationActivityUpdateReqVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.combination.vo.product.CombinationProductBaseVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.combination.vo.product.CombinationProductRespVO;
+import cn.iocoder.yudao.module.promotion.dal.dataobject.combination.CombinationActivityDO;
+import cn.iocoder.yudao.module.promotion.dal.dataobject.combination.CombinationProductDO;
+import cn.iocoder.yudao.module.promotion.dal.dataobject.combination.CombinationRecordDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.Map;
+
+import static cn.iocoder.yudao.framework.common.util.collection.CollectionUtils.convertMap;
+
+/**
+ * 拼团活动 Convert
+ *
+ * @author HUIHUI
+ */
+@Mapper
+public interface CombinationActivityConvert {
+
+    CombinationActivityConvert INSTANCE = Mappers.getMapper(CombinationActivityConvert.class);
+
+    CombinationActivityDO convert(CombinationActivityCreateReqVO bean);
+
+    CombinationActivityDO convert(CombinationActivityUpdateReqVO bean);
+
+    CombinationActivityRespVO convert(CombinationActivityDO bean);
+
+    CombinationProductRespVO convert(CombinationProductDO bean);
+
+    default CombinationActivityRespVO convert(CombinationActivityDO activity, List<CombinationProductDO> products) {
+        return convert(activity).setProducts(convertList2(products));
+    }
+
+    List<CombinationActivityRespVO> convertList(List<CombinationActivityDO> list);
+
+    PageResult<CombinationActivityRespVO> convertPage(PageResult<CombinationActivityDO> page);
+
+    default PageResult<CombinationActivityRespVO> convertPage(PageResult<CombinationActivityDO> page,
+                                                              List<CombinationProductDO> productList,
+                                                              List<ProductSpuRespDTO> spuList) {
+        Map<Long, ProductSpuRespDTO> spuMap = convertMap(spuList, ProductSpuRespDTO::getId);
+        PageResult<CombinationActivityRespVO> pageResult = convertPage(page);
+        pageResult.getList().forEach(item -> {
+            MapUtils.findAndThen(spuMap, item.getSpuId(), spu -> {
+                item.setSpuName(spu.getName());
+                item.setPicUrl(spu.getPicUrl());
+            });
+            item.setProducts(convertList2(productList));
+        });
+        return pageResult;
+    }
+
+    List<CombinationProductRespVO> convertList2(List<CombinationProductDO> productDOs);
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "activityId", source = "activity.id"),
+            @Mapping(target = "spuId", source = "activity.spuId"),
+            @Mapping(target = "skuId", source = "product.skuId"),
+            @Mapping(target = "combinationPrice", source = "product.combinationPrice"),
+            @Mapping(target = "activityStartTime", source = "activity.startTime"),
+            @Mapping(target = "activityEndTime", source = "activity.endTime")
+    })
+    CombinationProductDO convert(CombinationActivityDO activity, CombinationProductBaseVO product);
+
+    default List<CombinationProductDO> convertList(List<? extends CombinationProductBaseVO> products, CombinationActivityDO activity) {
+        return CollectionUtils.convertList(products, item -> convert(activity, item).setActivityStatus(activity.getStatus()));
+    }
+
+    default List<CombinationProductDO> convertList(List<CombinationProductBaseVO> updateProductVOs,
+                                                   List<CombinationProductDO> products, CombinationActivityDO activity) {
+        Map<Long, Long> productMap = convertMap(products, CombinationProductDO::getSkuId, CombinationProductDO::getId);
+        return CollectionUtils.convertList(updateProductVOs, updateProductVO -> convert(activity, updateProductVO)
+                .setId(productMap.get(updateProductVO.getSkuId()))
+                .setActivityStatus(activity.getStatus()));
+    }
+
+    CombinationRecordDO convert(CombinationRecordCreateReqDTO reqDTO);
+
+    List<CombinationRecordRespDTO> convert(List<CombinationRecordDO> bean);
+
+}
---
+++ yudao-module-mall/yudao-module-promotion-biz/src/main/java/cn/iocoder/yudao/module/promotion/convert/seckill/seckillactivity/SeckillActivityConvert.java
@@ -0,0 +1,82 @@
+package cn.iocoder.yudao.module.promotion.convert.seckill.seckillactivity;
+
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.framework.common.util.collection.CollectionUtils;
+import cn.iocoder.yudao.framework.common.util.collection.MapUtils;
+import cn.iocoder.yudao.module.product.api.spu.dto.ProductSpuRespDTO;
+import cn.iocoder.yudao.module.promotion.controller.admin.seckill.vo.activity.SeckillActivityCreateReqVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.seckill.vo.activity.SeckillActivityDetailRespVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.seckill.vo.activity.SeckillActivityRespVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.seckill.vo.activity.SeckillActivityUpdateReqVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.seckill.vo.product.SeckillProductBaseVO;
+import cn.iocoder.yudao.module.promotion.controller.admin.seckill.vo.product.SeckillProductRespVO;
+import cn.iocoder.yudao.module.promotion.dal.dataobject.seckill.seckillactivity.SeckillActivityDO;
+import cn.iocoder.yudao.module.promotion.dal.dataobject.seckill.seckillactivity.SeckillProductDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 秒杀活动 Convert
+ *
+ * @author 芋道源码
+ */
+@Mapper
+public interface SeckillActivityConvert {
+
+    SeckillActivityConvert INSTANCE = Mappers.getMapper(SeckillActivityConvert.class);
+
+    SeckillActivityDO convert(SeckillActivityCreateReqVO bean);
+
+    SeckillActivityDO convert(SeckillActivityUpdateReqVO bean);
+
+    SeckillActivityRespVO convert(SeckillActivityDO bean);
+
+    List<SeckillActivityRespVO> convertList(List<SeckillActivityDO> list);
+
+    PageResult<SeckillActivityRespVO> convertPage(PageResult<SeckillActivityDO> page);
+
+    default PageResult<SeckillActivityRespVO> convertPage(PageResult<SeckillActivityDO> page,
+                                                          List<SeckillProductDO> seckillProducts,
+                                                          List<ProductSpuRespDTO> spuList) {
+        PageResult<SeckillActivityRespVO> pageResult = convertPage(page);
+        // 拼接商品
+        Map<Long, ProductSpuRespDTO> spuMap = CollectionUtils.convertMap(spuList, ProductSpuRespDTO::getId);
+        pageResult.getList().forEach(item -> {
+            item.setProducts(convertList2(seckillProducts));
+            MapUtils.findAndThen(spuMap, item.getSpuId(),
+                    spu -> item.setSpuName(spu.getName()).setPicUrl(spu.getPicUrl()));
+        });
+        return pageResult;
+    }
+
+    SeckillActivityDetailRespVO convert1(SeckillActivityDO activity);
+
+    default SeckillActivityDetailRespVO convert(SeckillActivityDO activity, List<SeckillProductDO> products) {
+        return convert1(activity).setProducts(convertList2(products));
+    }
+
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "activityId", source = "activity.id"),
+            @Mapping(target = "configIds", source = "activity.configIds"),
+            @Mapping(target = "spuId", source = "activity.spuId"),
+            @Mapping(target = "skuId", source = "product.skuId"),
+            @Mapping(target = "seckillPrice", source = "product.seckillPrice"),
+            @Mapping(target = "stock", source = "product.stock"),
+            @Mapping(target = "activityStartTime", source = "activity.startTime"),
+            @Mapping(target = "activityEndTime", source = "activity.endTime")
+    })
+    SeckillProductDO convert(SeckillActivityDO activity, SeckillProductBaseVO product);
+
+    default List<SeckillProductDO> convertList(List<? extends SeckillProductBaseVO> products, SeckillActivityDO activity) {
+        return CollectionUtils.convertList(products, item -> convert(activity, item).setActivityStatus(activity.getStatus()));
+    }
+
+    List<SeckillProductRespVO> convertList2(List<SeckillProductDO> list);
+
+}
